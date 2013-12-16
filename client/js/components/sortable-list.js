b445966a2418e6b9bfbc9991157a5a5de9e341cb
Garth Williams <garth@wi.llia.ms>
2013-12-16 19:40:59 +0100

    Improved reliability of moving cards

--- client/js/components/sortable-list.js
+++ client/js/components/sortable-list.js
@@ -1,36 +1,5 @@
 require('./sortable-list-item');
 
-var updateSortedItems = function () {
-  var sort = 0;
-  var list = this.getProperties('sortKey', 'autoSave', 'parent', 'childrenKey', 'parentKey');
-  if (list.sortKey) {
-    _.each(this.$('li'), function (item) {
-      var child = item.sortableItem;
-      if (child) {
-        // set the order
-        child.set(list.sortKey, sort++);
-        // update the parent
-        if (list.parent && list.parentKey &&
-          child.get(list.parentKey) !== list.parent) {
-          // update the parents childrent collections
-          if (list.childrenKey) {
-            var collectionPath = list.childrenKey + '.content';
-            child.get(list.parentKey + '.' + collectionPath).removeObject(child);
-            list.parent.get(collectionPath).pushObject(child);
-          }
-          // update the childs parent
-          child.set(list.parentKey, list.parent);
-        }
-        // save changes
-        if (list.autoSave && child.get('isDirty')) {
-          console.log('saving', child);
-          child.save();
-        }
-      }
-    });
-  }
-};
-
 App.SortableListComponent = Ember.Component.extend({
   tagName: 'ul',
   classNameBindings: ['connectWith'],
@@ -43,20 +12,43 @@ App.SortableListComponent = Ember.Component.extend({
   connectWith: null,
 
   setup: function () {
-    //console.log('col ready', Ember.$('.board-lane-list'));
     var self = this;
-    if (this.get('connectWith')) {
-      this.$().on('sortupdate', function (event, ui) {
-        updateSortedItems.apply(self);
-      });
-    }
-    else {
-      this.$().sortable({
-        axis: 'y',
-        update: function(event, ui) {
-          updateSortedItems.apply(self);
+    var connectWith = this.get('connectWith');
+    this.$().sortable({
+      update: function(event, ui) {
+        var sort = 0;
+        var list = self.getProperties('sortKey', 'autoSave', 'parent', 'childrenKey', 'parentKey');
+        if (list.sortKey) {
+          _.each(self.$('li'), function (item) {
+            var child = item.sortableItem;
+            if (child) {
+              // set the order
+              child.set(list.sortKey, sort++);
+              // update the parent
+              var movedLane = false;
+              if (list.parent && list.parentKey &&
+                child.get(list.parentKey) !== list.parent) {
+                // update the parents childrent collections
+                if (list.childrenKey) {
+                  var collectionPath = list.childrenKey + '.content';
+                  child.get(list.parentKey + '.' + collectionPath).removeObject(child);
+                  list.parent.get(collectionPath).pushObject(child);
+                }
+                // update the childs parent
+                child.set(list.parentKey, list.parent);
+                movedLane = true;
+              }
+              // save changes
+              if (list.autoSave && (movedLane || child.get('isDirty'))) {
+                child.save();
+              }
+            }
+          });
         }
-      });
+      }
+    });
+    if (connectWith) {
+      Ember.$('.ui-sortable.' + connectWith).sortable('option', 'connectWith', '.' + connectWith);
     }
   }.on('didInsertElement'),
 
