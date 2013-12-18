48ca9e3213635d824db97eff809f78d6894265b0
Garth Williams <garth@wi.llia.ms>
2013-12-18 14:31:08 +0100

    Update cards in store based on changes sent from server

--- client/js/models/card.js
+++ client/js/models/card.js
@@ -16,15 +16,39 @@ App.Card = DS.Model.extend({
 
 App.serverEvents.addEventListener('createCard', function(e) {
   var store = App.Card.store;
-  console.log('create', e.data, store);
+  var cardData = JSON.parse(e.data).document;
+  var lane = store.getById('lane', cardData.lane);
+  // add the card if the lane is in the store
+  if (lane) {
+    var card = store.push('card', cardData);
+    lane.get('cards.content').pushObject(card);
+  }
 }, false);
 
 App.serverEvents.addEventListener('updateCard', function(e) {
   var store = App.Card.store;
-  console.log('update', e.data, store);
+  var cardData = JSON.parse(e.data).document;
+  var card = store.getById('card', cardData.id);
+  // update the card if it's in the store
+  if (card) {
+    var oldLane = card.get('lane');
+    var newLane = store.getById('lane', cardData.lane);
+    card = store.push('card', cardData);
+    // move lane if necessary
+    if (oldLane.get('id') !== newLane.get('id')) {
+      oldLane.get('cards.content').removeObject(card);
+      newLane.get('cards.content').pushObject(card);
+    }
+  }
 }, false);
 
 App.serverEvents.addEventListener('deleteCard', function(e) {
   var store = App.Card.store;
-  console.log('delete', e.data, store);
+  var cardData = JSON.parse(e.data).document;
+  var card = store.getById('card', cardData.id);
+  // remove the card from the store
+  if (card) {
+    card.get('lane.cards.content').removeObject(card);
+    store.unloadRecord(card);
+  };
 }, false);
