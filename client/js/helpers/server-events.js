d103bfe35aba6466f75da7ecebd30f1089f6cdbf
Garth Williams <garth@wi.llia.ms>
2013-12-18 15:22:04 +0100

    Don't console.log client connections

--- client/js/helpers/server-events.js
+++ client/js/helpers/server-events.js
@@ -5,15 +5,15 @@ App.serverEvents.addEventListener('ping', function(e) {
   // keep alive message from server
 }, false);
 
-App.serverEvents.addEventListener('open', function(e) {
-  console.log('/api/events connection open', e);
-}, false);
+// App.serverEvents.addEventListener('open', function(e) {
+//   console.log('/api/events connection open', e);
+// }, false);
 
-App.serverEvents.addEventListener('error', function(e) {
-  if (e.readyState === window.EventSource.CLOSED) {
-    console.log('/api/events connection closed', e);
-  }
-  else {
-    console.log('/api/events error', e);
-  }
-}, false);
+// App.serverEvents.addEventListener('error', function(e) {
+//   if (e.readyState === window.EventSource.CLOSED) {
+//     console.log('/api/events connection closed', e);
+//   }
+//   else {
+//     console.log('/api/events error', e);
+//   }
+// }, false);
