1bb8457da13f7913cf5965a6926f9b19a967c826
Garth Williams <garth@wi.llia.ms>
2014-01-06 23:24:50 +0100

    Double click a card to edit

--- client/js/views/card.js
+++ client/js/views/card.js
@@ -1,7 +1,15 @@
 App.BoardsCardView = Ember.View.extend({
   setup: function () {
+    // auto focus the first input
     if (this.controller.get('model.isEditing')) {
       this.$('input')[0].focus();
     }
+    // expand closed cards on double click
+    var self = this;
+    this.$().dblclick(function () {
+      if (!self.controller.get('model.isEditing')) {
+        self.controller.send('open');
+      }
+    });
   }.on('didInsertElement')
 });
