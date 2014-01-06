a6c3b4b12e341d6406b2862c8e762c0b89d25822
Garth Williams <garth@wi.llia.ms>
2014-01-06 16:17:27 +0100

    When sorting cards, ensure that sort observes card type priority

--- client/js/controllers/lane.js
+++ client/js/controllers/lane.js
@@ -1,8 +1,21 @@
 require('../views/lane');
 
 App.BoardsLaneController = Ember.ObjectController.extend({
-  sortedCards: function () {
-    var cards = Ember.A(this.get('model.cards'));
-    return cards.sortBy('priority', 'order');
-  }.property('model.cards.@each.priority', 'model.cards.@each.order')
+  sortedCards: null,
+
+  cardsChanged: function () {
+    Ember.run.once(this, 'sortCards');
+  }.observes('content.cards.@each.priority', 'content.cards.@each.order').on('init'),
+
+  sortCards: function () {
+    var list = this.get('model.cards').sortBy('priority', 'order');
+    for (var order = 0; order < list.length; order++) {
+      var card = list[order];
+      if (card.get('order') !== order) {
+        card.set('order', order);
+        card.save();
+      }
+    }
+    this.set('sortedCards', list);
+  }
 });
