127a212466b62d5d2947183f006bd5d295a46174
Garth Williams <garth@wi.llia.ms>
2014-01-08 08:36:54 +0100

    Use production emberjs version in production build

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,45 @@ var request = require('request');
 var portscanner = require('portscanner');
 var _ = require('underscore');
 
+var components = [
+  'bower_components/jquery/jquery.js',
+  'bower_components/jqueryui/ui/jquery.ui.effect.js',
+  'bower_components/jqueryui/ui/jquery.ui.effect-shake.js',
+  'bower_components/jqueryui/ui/jquery.ui.core.js',
+  'bower_components/jqueryui/ui/jquery.ui.widget.js',
+  'bower_components/jqueryui/ui/jquery.ui.mouse.js',
+  'bower_components/jqueryui/ui/jquery.ui.sortable.js',
+  'bower_components/bootstrap/js/transition.js',
+  'bower_components/bootstrap/js/alert.js',
+  'bower_components/bootstrap/js/button.js',
+  'bower_components/bootstrap/js/carousel.js',
+  'bower_components/bootstrap/js/collapse.js',
+  'bower_components/bootstrap/js/dropdown.js',
+  'bower_components/bootstrap/js/modal.js',
+  'bower_components/bootstrap/js/scrollspy.js',
+  'bower_components/bootstrap/js/tab.js',
+  'bower_components/bootstrap/js/tooltip.js',
+  'bower_components/bootstrap/js/popover.js',
+  'bower_components/bootstrap/js/affix.js',
+  'bower_components/typeahead.js/dist/typeahead.js',
+  'bower_components/bootstrap-tagsinput/dist/bootstrap-tagsinput.js',
+  'bower_components/keymaster/keymaster.js',
+  'node_modules/underscore/underscore.js',
+  'node_modules/faye/browser/faye-browser.js',
+  'bower_components/momentjs/moment.js',
+  'bower_components/toastr/toastr.js'
+];
+var debugComponents = _.union(components, [
+  'bower_components/handlebars/handlebars.js',
+  'bower_components/ember/ember.js',
+  'bower_components/ember-data/ember-data.js'
+]);
+var productionComponents = _.union(components, [
+  'bower_components/handlebars/handlebars.runtime.js',
+  'bower_components/ember/ember.prod.js',
+  'bower_components/ember-data/ember-data.prod.js'
+]);
+
 module.exports = function (grunt) {
 
   require('time-grunt')(grunt);
@@ -34,40 +73,14 @@ module.exports = function (grunt) {
       options: {
         separator: ';',
       },
-      dist: {
-        src: [
-          'bower_components/jquery/jquery.js',
-          'bower_components/jqueryui/ui/jquery.ui.effect.js',
-          'bower_components/jqueryui/ui/jquery.ui.effect-shake.js',
-          'bower_components/jqueryui/ui/jquery.ui.core.js',
-          'bower_components/jqueryui/ui/jquery.ui.widget.js',
-          'bower_components/jqueryui/ui/jquery.ui.mouse.js',
-          'bower_components/jqueryui/ui/jquery.ui.sortable.js',
-          'bower_components/bootstrap/js/transition.js',
-          'bower_components/bootstrap/js/alert.js',
-          'bower_components/bootstrap/js/button.js',
-          'bower_components/bootstrap/js/carousel.js',
-          'bower_components/bootstrap/js/collapse.js',
-          'bower_components/bootstrap/js/dropdown.js',
-          'bower_components/bootstrap/js/modal.js',
-          'bower_components/bootstrap/js/scrollspy.js',
-          'bower_components/bootstrap/js/tab.js',
-          'bower_components/bootstrap/js/tooltip.js',
-          'bower_components/bootstrap/js/popover.js',
-          'bower_components/bootstrap/js/affix.js',
-          'bower_components/typeahead.js/dist/typeahead.js',
-          'bower_components/bootstrap-tagsinput/dist/bootstrap-tagsinput.js',
-          'bower_components/handlebars/handlebars.js',
-          'bower_components/ember/ember.js',
-          'bower_components/ember-data/ember-data.js',
-          'bower_components/keymaster/keymaster.js',
-          'node_modules/underscore/underscore.js',
-          'node_modules/faye/browser/faye-browser.js',
-          'bower_components/momentjs/moment.js',
-          'bower_components/toastr/toastr.js'
-        ],
+      debug: {
+        src: debugComponents,
         dest: 'public/js/vendor.js',
       },
+      production: {
+        src: productionComponents,
+        dest: 'public/js/vendor.js',
+      }
     },
     recess: {
       dist: {
@@ -86,7 +99,7 @@ module.exports = function (grunt) {
             expand: true,
             flatten: true,
             // include all font files
-            src: ['components/font-awesome/fonts/*'],
+            src: ['bower_components/font-awesome/fonts/*'],
             dest: 'public/fonts/',
             filter: 'isFile'
           }
@@ -139,7 +152,7 @@ module.exports = function (grunt) {
       },
       vendor: {
         files: ['components/**/*.js'],
-        tasks: ['concat']
+        tasks: ['concat:debug']
       },
       templates: {
         files: ['client/templates/**/*.hbs'],
@@ -233,7 +246,7 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   grunt.registerTask('build', [
-    'emberTemplates', 'recess', 'browserify', 'concat', 'copy'
+    'emberTemplates', 'recess', 'browserify', 'concat:debug', 'copy'
   ]);
   grunt.registerTask('server', [
     'jshint', 'build', 'develop:server', 'exec:runTests', 'watch'
@@ -254,7 +267,7 @@ module.exports = function (grunt) {
     'develop:testServer', 'waitForPort', 'mochaTest'
   ]);
   grunt.registerTask('production', [
-    'jshint', 'build', 'uglify', 'cssmin'
+    'jshint', 'build', 'concat:production', 'uglify', 'cssmin'
   ]);
 
   grunt.registerTask('default', ['test']);
