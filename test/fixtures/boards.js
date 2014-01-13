2d250a5d316cc65efdc4ab93b66982647a393ad6
Garth Williams <garth@wi.llia.ms>
2014-01-13 13:40:26 +0100

    Updated test fixtures to match updated models

--- test/fixtures/boards.js
+++ test/fixtures/boards.js
@@ -4,15 +4,153 @@ module.exports = [
   {
     _id: new ObjectId("22875455e3e2812b6e000001"),
     name: 'Board',
-    defaultCardType: new ObjectId("72875455e3e2812b6e000001"),
-    createdByUser: new ObjectId("12875455e3e2812b6e000001"),
-    createdOn: new Date('2013-11-20')
+    defaultCardTypeId: new ObjectId("72875455e3e2812b6e000001"),
+    createdByUserId: new ObjectId("12875455e3e2812b6e000001"),
+    createdOn: new Date('2013-11-20'),
+    cardTypes: [
+      {
+        _id: new ObjectId("72875455e3e2812b6e000001"),
+        name: 'Story',
+        icon: 'book',
+        pointScale: '1,2,3,5,8',
+        priority: 0
+      },
+      {
+        _id: new ObjectId("72875455e3e2812b6e000002"),
+        name: 'Bug',
+        icon: 'bug',
+        pointScale: '',
+        priority: 1
+      },
+      {
+        _id: new ObjectId("72875455e3e2812b6e000003"),
+        name: 'Task',
+        icon: 'wrench',
+        pointScale: '',
+        priority: 0
+      }
+    ],
+    lanes: [
+      {
+        _id: new ObjectId("32875455e3e2812b6e000001"),
+        name: 'One',
+        order: 1,
+        cards: [
+          {
+            _id: new ObjectId("42875455e3e2812b6e000001"),
+            cardTypeId: new ObjectId("72875455e3e2812b6e000001"),
+            title: 'Item 1',
+            description: 'some details',
+            points: 1,
+            createdByUserId: new ObjectId("12875455e3e2812b6e000001"),
+            createdOn: new Date('2013-11-20'),
+            assignedToUserId: null,
+            order: 1,
+            comments: [
+              {
+                _id: new ObjectId("52875455e3e2812b6e000001"),
+                text: 'I like it',
+                userId: new ObjectId("12875455e3e2812b6e000001"),
+                createdOn: new Date('2013-11-20')
+              },
+              {
+                _id: new ObjectId("52875455e3e2812b6e000002"),
+                text: 'I don\'t like it',
+                userId: new ObjectId("12875455e3e2812b6e000001"),
+                createdOn: new Date('2013-11-21')
+              }
+            ]
+          },
+          {
+            _id: new ObjectId("42875455e3e2812b6e000002"),
+            cardTypeId: new ObjectId("72875455e3e2812b6e000001"),
+            title: 'Item 2',
+            description: 'some details',
+            points: 1,
+            createdByUserId: new ObjectId("12875455e3e2812b6e000001"),
+            createdOn: new Date('2013-11-20'),
+            assignedToUserId: null,
+            order: 2,
+            comments: [
+              {
+                _id: new ObjectId("52875455e3e2812b6e000003"),
+                text: 'Super',
+                userId: new ObjectId("12875455e3e2812b6e000001"),
+                createdOn: new Date('2013-11-21')
+              }
+            ]
+          },
+          {
+            _id: new ObjectId("42875455e3e2812b6e000003"),
+            cardTypeId: new ObjectId("72875455e3e2812b6e000001"),
+            title: 'Item 3',
+            description: 'some details',
+            points: 1,
+            createdByUserId: new ObjectId("12875455e3e2812b6e000001"),
+            createdOn: new Date('2013-11-20'),
+            assignedToUserId: null,
+            order: 3
+          },
+          {
+            _id: new ObjectId("42875455e3e2812b6e000004"),
+            cardTypeId: new ObjectId("72875455e3e2812b6e000001"),
+            title: 'Item 4',
+            description: 'some details',
+            points: 1,
+            createdByUserId: new ObjectId("12875455e3e2812b6e000001"),
+            createdOn: new Date('2013-11-20'),
+            assignedToUserId: null,
+            order: 4
+          }
+        ]
+      },
+      {
+        _id: new ObjectId("32875455e3e2812b6e000002"),
+        name: 'Two',
+        order: 2,
+        cards: [
+          {
+            _id: new ObjectId("42875455e3e2812b6e000005"),
+            cardTypeId: new ObjectId("72875455e3e2812b6e000001"),
+            title: 'Item 5',
+            description: 'some details',
+            points: 1,
+            createdByUserId: new ObjectId("12875455e3e2812b6e000001"),
+            createdOn: new Date('2013-11-20'),
+            assignedToUserId: null,
+            order: 1
+          }
+        ]
+      },
+      {
+        _id: new ObjectId("32875455e3e2812b6e000003"),
+        name: 'Three',
+        order: 3
+      }
+    ]
   },
   {
     _id: new ObjectId("22875455e3e2812b6e000002"),
     name: 'Board An Other',
-    defaultCardType: new ObjectId("72875455e3e2812b6e000004"),
-    createdByUser: new ObjectId("12875455e3e2812b6e000001"),
-    createdOn: new Date('2013-11-20')
+    defaultCardTypeId: new ObjectId("72875455e3e2812b6e000004"),
+    createdByUserId: new ObjectId("12875455e3e2812b6e000001"),
+    createdOn: new Date('2013-11-20'),
+    cardTypes: [
+      {
+        _id: new ObjectId("72875455e3e2812b6e000004"),
+        name: 'Story',
+        icon: 'book',
+        pointScale: '1,2,3,5,8',
+        priority: 0
+      },
+    ],
+    lanes: [
+      {
+        _id: new ObjectId("32875455e3e2812b6e000004"),
+        board: new ObjectId("22875455e3e2812b6e000002"),
+        name: 'One',
+        order: 1
+      }
+    ]
   }
 ];
--- test/fixtures/users.js
+++ test/fixtures/users.js
@@ -4,11 +4,15 @@ module.exports = [
   {
     _id: new ObjectId("12875455e3e2812b6e000001"),
     name: 'Garth',
+    initials: 'G',
+    login: 'garth',
     password: 'sha1$87eb07a6$1$40c17198317550debf1a7cf19bf793d8d97b51e3', //test
   },
   {
     _id: new ObjectId("12875455e3e2812b6e000002"),
     name: 'Brian',
+    initials: 'B',
+    login: 'brian',
     password: 'sha1$87eb07a6$1$40c17198317550debf1a7cf19bf793d8d97b51e3', //test
   }
 ];
