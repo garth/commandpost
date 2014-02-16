a6f43c812ed69e670b4000765fb17800a5df4388
Garth Williams <garth@wi.llia.ms>
2014-02-16 14:33:03 +0100

    Ensure that modal outlet is always cleared

--- client/js/components/modal-dialog.js
+++ client/js/components/modal-dialog.js
@@ -1,17 +1,18 @@
 App.ModalDialogComponent = Ember.Component.extend({
 
   setup: function () {
-    this.$('.modal').modal('show');
+    var element = this.$('.modal');
+    element.modal('show');
+    var self = this;
+    element.on('hidden.bs.modal', function () {
+      self.sendAction('closeModal');
+      self.sendAction();
+    });
   }.on('didInsertElement'),
 
   actions: {
     close: function () {
       this.$('.modal').modal('hide');
-      var self = this;
-      setTimeout(function () {
-        self.sendAction('closeModal');
-        self.sendAction();
-      }, 300);
     }
   }
 });
